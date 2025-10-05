6a0d6a2bfa7becb890996886918f899421554514
alpharii <alphari404@gmail.com>
2025-10-05 11:57:57 +0700

    go: string, bool, number, variable

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("bintang")
+	fmt.Println("bintang alphari")
+	fmt.Println("muhammad bintang alphari")
+
+	fmt.Println(len("bintang"))
+	fmt.Println("bintang alphari"[0]) //dapet byte bukan char string
+	fmt.Println("muhammad bintang alphari"[1])
+}
---
+++ variable.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var name string
+
+	// name = "bintang alphari"
+	// fmt.Println(name)	
+
+	// var name = "bintang alphari"
+	// fmt.Println(name)
+
+	name := "bintang alphari"
+	fmt.Println(name)
+	
+	name = "muhammad alphari"
+	fmt.Println(name)
+
+	var (
+		firstName = "bintang"
+		lastName = "alphari"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
